7d948e460065778ca9ed58c704087f78b10b4cda
Avinash Verma <avinashverma.it@gmail.com>
2024-06-12 18:47:22 +0530

    Feat: implement drag and drop

--- src/components/kanban-board/KanbanBoardPanels.tsx
+++ src/components/kanban-board/KanbanBoardPanels.tsx
@@ -1,10 +1,11 @@
 import { Flex, IconButton, Text } from '@radix-ui/themes';
 
-import { TaskStatus, useTasks } from '@/hooks/use-tasks';
+import { Task, TaskStatus, useTasks } from '@/hooks/use-tasks';
 import { getStatusText } from '@/helpers';
 import { TaskCard } from '../task-card';
 import { PlusIcon } from '@radix-ui/react-icons';
 import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
 interface TaskContainerProps {
   status: TaskStatus;
@@ -36,7 +37,32 @@ const TaskContainer: React.FC<TaskContainerProps> = ({ status }) => {
 };
 
 export const KanbanBoardPanels = () => {
-  const { statuses } = useTasks();
+  const { statuses, tasks, updateTask } = useTasks();
+  const [isDragging, setIsDragging] = React.useState(false);
+
+  const onDrop = async (
+    event: React.DragEvent<HTMLDivElement>,
+    status: TaskStatus
+  ) => {
+    setIsDragging(false);
+
+    const taskId = event.dataTransfer.getData('taskId') as Task['id'];
+    const task = tasks.find(({ id }) => taskId === id);
+
+    if (!task) return;
+    // Do nothing if task is being moved to same status
+    if (task.status === status) return;
+
+    await updateTask(taskId, {
+      ...task,
+      status,
+    });
+  };
+
+  const onDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
 
   return statuses.map((status) => (
     <Flex
@@ -50,7 +76,11 @@ export const KanbanBoardPanels = () => {
         borderRadius: '4px',
         padding: '8px',
         flexGrow: 1,
+        position: 'relative',
+        border: isDragging ? '1px dashed #aaa' : '',
       }}
+      onDrop={(e) => onDrop(e, status)}
+      onDragOver={onDragOver}
     >
       <TaskContainer status={status} />
     </Flex>
